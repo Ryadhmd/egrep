0be3b74ce356381123418be483c20faa9125b120
Cherif Hamdini <ryadhamdini@gmail.com>
2023-10-07 13:18:27 +0200

    created a tests folder

--- tests/test_dfa.py
+++ tests/test_dfa.py
@@ -2,7 +2,6 @@ import pytest
 from nfa_to_dfa import nfa_to_dfa,is_accepted_by_dfa
 from regex_to_nfa import regex_to_nfa
 
-
 # Define some test cases for is_accepted_by_dfa with different regex and input strings
 @pytest.mark.parametrize("regex, input_string, match_all, expected_result", [
     ("a.*b", "acccb", True, True),
